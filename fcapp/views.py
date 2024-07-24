5e4d8427ba0b774d901969681508f0314f7fe93a
unknown <abnvarghese@gmail.com>
2024-07-24 23:17:06 +0530

    second

--- fcapp/views.py
+++ fcapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'index.html')
+    return render(request,'home.html')
 
 def signup(request):
     return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
