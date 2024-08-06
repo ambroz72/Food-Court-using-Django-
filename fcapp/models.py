47eff979670a419cc095c2a6d8d52e709cf1559e
unknown <abnvarghese@gmail.com>
2024-08-06 21:55:10 +0530

    agu 6

--- fcapp/models.py
+++ fcapp/models.py
@@ -1,3 +1,63 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from pydoc import describe
 
 # Create your models here.
+class Category(models.Model):       #admin....
+    product_category = models.CharField(max_length=100,null=True)
+    product_price = models.IntegerField(null=True)
+    product_stock = models.IntegerField(null=True)
+    
+class User(models.Model):       #user....
+    e_category =models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    e_user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    e_fname = models.CharField(max_length=30,null=True)
+    e_lname = models.CharField(max_length=30,null=True)
+    e_address = models.CharField(max_length=100,null=True)
+    e_phone_numbr = models.BigIntegerField(10,null=True)
+    e_email = models.EmailField(null=True)
+
+class Product(models.Model):
+    p_category =models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    p_user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    product_name = models.CharField(max_length=30,null=True)
+    product_desc = models.TextField(max_length=100,null=True)
+    product_price = models.IntegerField(null=True)
+    product_img = models.ImageField(upload_to='img/',null=True)
+    product_qty =models.IntegerField(null=True)
+    
+class Feedback(models.Model): 
+    f_user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    f_name=models.CharField(max_length=50,null=True)
+    f_feed=models.CharField(max_length=100,null=True)
+    f_DOB=models.DateField(null=True)
+    f_status=models.CharField(max_length=30,null=True)
+    
+class Cart(models.Model):
+    cart_id=models.CharField(max_length=250,blank=True,null=True)
+    date_added=models.DateField(auto_now_add=True,null=True)
+    
+    class Meta:
+        db_table='Cart'
+        ordering=['date_added']
+        
+    def __str__(self):
+        return '{}'.format (self.cart_id)
+    
+class CartItem(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE,null=True)
+    quantity=models.IntegerField(null=True)
+    active=models.BooleanField(default=True,null=True)
+    class Meta:
+        db_table='CartItem'
+    def sub_total(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return '{}'.format (self.product)
+    
+    
+
--- fcapp/views.py
+++ fcapp/views.py
@@ -4,7 +4,57 @@ def home(request):
     return render(request,'home.html')
 
 def signup(request):
+    
+    try:
+        if request.method == 'POST':
+            Fname = request.POST['fname']
+            Lname= request.POST['lname']
+            usernam= request.POST['uname']
+            Email = request.POST['E-mail']
+            Address=request.POST['adds']
+            paswd = request.POST['pswd']
+        if paswd:
+            if User.objects.filter(password = paswd).exists():
+                print("This user name already exists")
+                sweetify.error(request,"User name already exist")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=usernam,first_name=Fname,
+                            last_name=Lname,email=Email,password=paswd)
+                user.save()
+                custm = Employee( e_fname=Fname,
+                                                e_lname=Lname,
+                                                e_address=Address,
+                                                 e_age=Age,
+                                                e_email=Email,
+                                                e_user=employee )
+                custm.save()
+                sweetify.success(request,"Your account has been registered successfully.Please use the Login credetianls to login ")
+                return redirect('signup')
+          
+    except:
+            sweetify.error(request,"Some details seems to be missing or wrong. Please check again that all detials are entered correctly")
+            return redirect('signup')
+    
     return render(request,'signup.html')
 
 def login(request):
+    if request.method== 'POST':
+        global u_name
+    u_name = request.POST['logname']
+    pawd = request.POST['passw']
+    log= auth.authenticate(username = u_name, password = pawd)
+    if log is not None:
+        if log.is_staff:
+            auth.login(request,log)
+            sweetify.success(request,'Login successful')
+            return redirect('Eadmin')
+        else:
+            auth.login(request,log)
+            sweetify.success(request,'Login successful')
+            return redirect('Euser')
+    else:
+        sweetify.error(request,"User name or password does not match. Try again.")
+        return redirect('loginpage')
+    
     return render(request,'login.html')
